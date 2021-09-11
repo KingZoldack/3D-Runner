fb5a9306eaca2578c3b2ecf6019df4d9f3945029
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-09-11 08:42:04 +0100

    Script
    Added Level Generator Script and initializations.

---
+++ Assets/Scripts/Level Generator Scripts/LevelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	[SerializeField]
+	int _levelLenght, _startPlatformLenght = 5, _endPlatformLength = 5, _distanceBetweenPlatforms, _minPlatformLength, _maxPlatformLength;
+
+	[SerializeField]
+	float _minYPlatformPos = 0f, _maxYPlatformPos = 10f, _monsterSpawnRate = 0.25f, _collectableSpawnRate = 0.1f, _minYHealthCollectablePos, _maxYHealthCollectablePos;
+
+	[SerializeField]
+	Transform _platform, _platformParent, _monstaer, _monsterParent, _healthCollectable, _healthCollectableParent;
+
+	float _platformLastPosX;
+
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+}
