2ad7cb6cac6e7bb3c339c55a2de0d5e4bdd0576e
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-02 03:55:52 +0100

    Camera
    Made camera follow player.  Added scripts for movement and tags.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	Transform playerTarget;
+
+	[SerializeField]
+	float offsetZ = -15f, offsetX = -5f, constantY = 5f, cameraLerpTime = 0.05f;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		playerTarget = GameObject.FindGameObjectWithTag(Tags.Player_Tag).transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (playerTarget)
+        {
+			Vector3 targetPosition = new Vector3(playerTarget.position.x + offsetX, constantY, playerTarget.position.z + offsetZ);
+			transform.position = Vector3.Lerp(transform.position, targetPosition, cameraLerpTime);
+        }
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	float _movementSpeed = 5f;
+
+	Rigidbody _rb;
+
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		MovePlayer();
+	}
+
+	void MovePlayer()
+    {
+		_rb.velocity = new Vector3(_movementSpeed, _rb.velocity.y, 0f);
+    }
+}
